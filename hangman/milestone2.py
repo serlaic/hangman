e4293e72d181b45b82f3b87af35345f5d01e8fde
serlaic <s.laicuks@wesec.co.uk>
2023-02-11 16:34:25 +0000

    adds if statement which checks if the guess input is an alphabetical cahracter and number of characters is not higher than 1

--- hangman/milestone2.py
+++ hangman/milestone2.py
@@ -3,3 +3,8 @@ word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
 print(word_list)
 word = random.choice(word_list)
 print(word)
+guess = input('Please choose a single letter:')
+if len(guess) == 1 and guess.isalpha():
+    print('Goog guess!')
+else:
+    print('Oops! That is not a valid input.')
