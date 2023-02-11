3e4958ad3d7e3a312e268c71d28c290fd202439f
serlaic <s.laicuks@wesec.co.uk>
2023-02-11 20:04:40 +0000

    just a test

--- milestone2.py
+++ milestone2.py
@@ -1,10 +1,3 @@
-import random
-word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
+word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry']
 print(word_list)
-word = random.choice(word_list)
-print(word)
-guess = input('Please choose a single letter:')
-if len(guess) == 1 and guess.isalpha():
-    print('Goog guess!')
-else:
-    print('Oops! That is not a valid input.')
+
