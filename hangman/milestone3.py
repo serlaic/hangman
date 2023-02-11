4adbbbc5e575c3ebcdc5ebb5b3867014c090df5e
serlaic <s.laicuks@wesec.co.uk>
2023-02-11 18:46:55 +0000

    adds another if statement to check if guess input matches any letter from word

--- hangman/milestone3.py
+++ hangman/milestone3.py
@@ -1,7 +1,17 @@
+import random
+
+word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
+print(word_list)
+word= random.choice(word_list)
+print(word)
+
 while True:
     guess = input('Please choose a single letter:')
     if len(guess) == 1 and guess.isalpha():
-        print('Goog guess!')
-        break
+        if guess in word:
+            print(f'Good guess, "{guess}", is in the word' )
+            break
+        else:
+            print(f'Sorry, "{guess}" is not in the word. Try again')
     else:
         print('Invalid letter. Please, enter a single alphabetical character.')
