cc8cd9f1213cab0658b7cecb942d705218f6dd30
serlaic <s.laicuks@wesec.co.uk>
2023-02-11 19:12:58 +0000

    separates previous code into a function check_guess and ask_for_input

--- milestone3.py
+++ milestone3.py
@@ -5,13 +5,22 @@ print(word_list)
 word= random.choice(word_list)
 print(word)
 
-while True:
-    guess = input('Please choose a single letter:')
-    if len(guess) == 1 and guess.isalpha():
-        if guess in word:
-            print(f'Good guess, "{guess}", is in the word' )
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f'Good guess, "{guess}", is in the word' )
+    else:
+        print(f'Sorry, "{guess}" is not in the word. Try again')
+
+
+def ask_for_input():
+    while True:
+        guess = input('Please choose a single letter:')
+        if len(guess) == 1 and guess.isalpha():
+            print('Ok')
             break
         else:
-            print(f'Sorry, "{guess}" is not in the word. Try again')
-    else:
-        print('Invalid letter. Please, enter a single alphabetical character.')
+            print('Invalid letter. Please, enter a single alphabetical character.')
+    check_guess(guess)
+
+ask_for_input()
