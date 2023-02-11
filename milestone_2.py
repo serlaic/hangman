2cdf5c0106b29c4b22979bdfe12759421058e7a9
serlaic <s.laicuks@wesec.co.uk>
2023-02-11 20:12:35 +0000

    adding all code to the file

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,10 @@
-word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry']
+import random
+word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
 print(word_list)
-
+word = random.choice(word_list)
+print(word)
+guess = input('Please choose a single letter:')
+if len(guess) == 1 and guess.isalpha():
+    print('Goog guess!')
+else:
+    print('Oops! That is not a valid input.')
