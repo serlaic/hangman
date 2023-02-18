cbbbb28f8b204dd17d051bc7aaea9849df0efa4b
serlaic <s.laicuks@wesec.co.uk>
2023-02-18 21:30:32 +0000

    creates check_guess and ask_for_input methods

--- milestone_4.py
+++ milestone_4.py
@@ -9,5 +9,24 @@ class Hangman:
         self.list_of_guesses = []
         self.num_letters = len(set(self.word))
 
-    
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f'Good guess, "{guess}", is in the word' )
+        else:
+            print(f'Sorry, "{guess}" is not in the word. Try again')
 
+    def ask_for_input(self):
+        while True:
+            guess = input('Please choose a single letter:')
+            if not (len(guess) == 1 and guess.isalpha()):
+                print('Invalid letter. Please, enter a single alphabetical character.')
+            elif guess in self.list_of_guesses:
+                print('You already tried that letter')
+            else:
+                print('OK')
+                break
+    
+word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry', 'watermelon']
+new_game = Hangman(word_list)
+new_game.ask_for_input()
