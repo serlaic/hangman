adefa81747827cc22d0844ac165e55f07e6d0d86
serlaic <s.laicuks@wesec.co.uk>
2023-02-18 21:09:06 +0000

    creates class Hangman and initialises certain attributes

---
+++ milestone_4.py
@@ -0,0 +1,13 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives = 5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for letter in self.word]
+        self.list_of_guesses = []
+        self.num_letters = len(set(self.word))
+
+    
+
