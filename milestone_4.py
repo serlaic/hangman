67a5eec0cf588eead04f3b9f0364010f52bc28b6
serlaic <s.laicuks@wesec.co.uk>
2023-02-19 14:56:44 +0000

    adds guess to list_of_guesses and reduces num_lives by 1 if no letters were guessed

--- milestone_4.py
+++ milestone_4.py
@@ -17,10 +17,15 @@ class Hangman:
                 if guess == self.word[word_id]:
                     self.word_guessed[word_id] = guess
             self.num_letters -= 1
+        else:
+            self.num_lives -=1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+        self.list_of_guesses.append(guess)
 
     def ask_for_input(self):
         while True:
-            guess = input("Please choose a single letter:")
+            guess = input("Please choose a single alphabetical character:")
             if not (len(guess) == 1 and guess.isalpha()):
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif guess in self.list_of_guesses:
